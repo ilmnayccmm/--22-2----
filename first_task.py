7b85a5de88a6271a8ed4aae9880267cf80665122
stakhniukbohdan <bstakhnuik@gmail.com>
2025-02-04 13:19:47 +0200

    added name print

--- first_task.py
+++ first_task.py
@@ -1,2 +1,5 @@
 a = input("Hi, my name is Havkaliuk Julia and I’m studying software development in college")
 print(a)
+
+b = input("Hi, my name is Hryniuk Olya and I’m studying software development in college")
+print(b)
